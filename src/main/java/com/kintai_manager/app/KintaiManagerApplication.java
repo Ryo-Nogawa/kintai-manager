af1994e1e6afcad30d74fd50d32e6bdccadac1bf
Ryo Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2025-11-12 19:07:09 +0900

    first commit

---
+++ src/main/java/com/kintai_manager/app/KintaiManagerApplication.java
@@ -0,0 +1,13 @@
+package com.kintai_manager.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KintaiManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KintaiManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kintai_manager/app/KintaiManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kintai_manager.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KintaiManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
