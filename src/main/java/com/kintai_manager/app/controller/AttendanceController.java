6c1c4d25e4b87f2cc18d0a5368c6fcc888c6d023
Ryo Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2025-11-14 00:03:27 +0900

    DIによるService呼び出し(#3)

--- src/main/java/com/kintai_manager/app/controller/AttendanceController.java
+++ src/main/java/com/kintai_manager/app/controller/AttendanceController.java
@@ -5,12 +5,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.kintai_manager.app.service.RegistService;
+
+import lombok.RequiredArgsConstructor;
+
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/kintai-manager/attendance")
 public class AttendanceController {
 
+    private final RegistService registService;
+
+    // 勤務開始リクエスト
     @PostMapping("/start")
     public String startAttendance(Model model) {
+        // 開始時間を登録
+        registService.registAttendance("12345", "WORK_START");
         model.addAttribute("attendance", "勤務開始");
         return "attendance-done";
     }
