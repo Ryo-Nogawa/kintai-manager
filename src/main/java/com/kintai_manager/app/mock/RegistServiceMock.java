582cbb9dbeb01c6cd00e70b5cece76f92da895b3
Ryo Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2025-11-13 23:45:05 +0900

    登録処理のMock作成(#3)

---
+++ src/main/java/com/kintai_manager/app/mock/RegistServiceMock.java
@@ -0,0 +1,13 @@
+package com.kintai_manager.app.mock;
+
+import org.springframework.stereotype.Service;
+
+import com.kintai_manager.app.service.RegistService;
+
+@Service
+public class RegistServiceMock implements RegistService {
+    @Override
+    public void registAttendance(String employee_id, String event_type) {
+        System.out.println("employeeId: " + employee_id + " eventType: " + event_type);
+    }
+}
