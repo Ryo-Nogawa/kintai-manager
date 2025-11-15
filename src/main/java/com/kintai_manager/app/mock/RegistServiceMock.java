6713a19973cd6214ba85bd3c6d04eb46d1a69ebf
Ryo Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2025-11-15 21:29:45 +0900

    ServiceのパラメータをEntiryに修正したのでMockRepositoryも修正(#15)

--- src/main/java/com/kintai_manager/app/mock/RegistServiceMock.java
+++ src/main/java/com/kintai_manager/app/mock/RegistServiceMock.java
@@ -1,15 +1,18 @@
 package com.kintai_manager.app.mock;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
+import com.kintai_manager.app.entity.TimeEvent;
 import com.kintai_manager.app.service.RegistService;
 
 @Service
+@Profile("local")
 public class RegistServiceMock implements RegistService {
     @Override
-    public void registAttendance(String employee_id, String event_type) {
+    public void registAttendance(TimeEvent timeEvent) {
         System.out.println("--------------------------------");
-        System.out.println("employeeId: " + employee_id + " eventType: " + event_type);
+        System.out.println("employeeId: " + timeEvent.getEmployeeId() + " eventType: " + timeEvent.getEventType());
         System.out.println("--------------------------------");
     }
 }
