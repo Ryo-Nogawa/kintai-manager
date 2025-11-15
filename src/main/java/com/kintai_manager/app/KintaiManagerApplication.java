a4c3ad7c80f66f15c1bf3e5a3c0ad10349dc5453
Ryo Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2025-11-15 11:56:34 +0900

    変数名のリファクタリング(#15)

--- src/main/java/com/kintai_manager/app/KintaiManagerApplication.java
+++ src/main/java/com/kintai_manager/app/KintaiManagerApplication.java
@@ -10,10 +10,10 @@ public class KintaiManagerApplication {
 
 	public static void main(String[] args) {
 		// 環境変数を読み込む
-		Dotenv env = Dotenv.configure().filename("local.env").ignoreIfMissing().load();
+		Dotenv environment_variables = Dotenv.configure().filename("local.env").ignoreIfMissing().load();
 
 		// 読み込んだ環境変数をシステムプロパティに設定する
-		env.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+		environment_variables.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 
 		SpringApplication.run(KintaiManagerApplication.class, args);
 	}
