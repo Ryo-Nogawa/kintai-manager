6c9a8d488bbf1f457e6d7204d72e5d1b0e8748d3
Ryo Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2025-11-15 21:30:30 +0900

    time_eventテーブル用のRepository作成(#15)

---
+++ src/main/java/com/kintai_manager/app/repository/TimeEventRepository.java
@@ -0,0 +1,7 @@
+package com.kintai_manager.app.repository;
+
+import com.kintai_manager.app.entity.TimeEvent;
+
+public interface TimeEventRepository {
+    void addTimeEvent(TimeEvent timeEvent);
+}
