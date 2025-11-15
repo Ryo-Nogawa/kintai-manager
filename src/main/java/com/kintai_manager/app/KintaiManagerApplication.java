670d7afd4ca73d249f328a64fe6b6bac00f4df31
Ryo Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2025-11-15 09:37:25 +0900

    mysqlの接続情報を追加(#15)

--- src/main/java/com/kintai_manager/app/KintaiManagerApplication.java
+++ src/main/java/com/kintai_manager/app/KintaiManagerApplication.java
@@ -3,10 +3,17 @@ package com.kintai_manager.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class KintaiManagerApplication {
 
 	public static void main(String[] args) {
+		// Load .env file
+		Dotenv dotenv = Dotenv.configure().filename("local.env").ignoreIfMissing().load();
+
+		// Set as system env properties
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(KintaiManagerApplication.class, args);
 	}
 
