7326d5ba90fef05888b686a9e5e1a9bd89d2f70c
Ryo Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2025-11-18 07:59:29 +0900

    登録リクエストのURLを一本化(#18)

--- src/main/java/com/kintai_manager/app/controller/AttendanceController.java
+++ src/main/java/com/kintai_manager/app/controller/AttendanceController.java
@@ -15,63 +15,18 @@ import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/kintai_manager/")
+@RequestMapping("/kintai_manager")
 public class AttendanceController {
 
     private final RegistService registService;
 
-    // 勤務開始リクエスト
-    @PostMapping("/work/start")
-    public String startWork(Model model) {
-        // 開始時間を登録
-        TimeEvent timeEvent = new TimeEvent();
-        timeEvent.setEmployeeId("00000");
-        timeEvent.setEventType("WORK_START");
-        registService.registAttendance(timeEvent);
-
-        model.addAttribute("attendance", "勤務開始");
-        return "attendance-done";
-    }
-
-    // 勤務終了リクエスト
-    @PostMapping("/work/end")
-    public String endWork(Model model) {
-        // 終了時間を登録
-        TimeEvent timeEvent = new TimeEvent();
-        timeEvent.setEmployeeId("00000");
-        timeEvent.setEventType("WORK_END");
-        registService.registAttendance(timeEvent);
-
-        model.addAttribute("attendance", "勤務終了");
-        return "attendance-done";
-    }
-
-    // 休憩開始リクエスト
-    @PostMapping("/break/start")
-    public String startBreak(Model model) {
-        // 休憩開始時間を登録
-        TimeEvent timeEvent = new TimeEvent();
-        timeEvent.setEmployeeId("00000");
-        timeEvent.setEventType("BREAK_START");
-        registService.registAttendance(timeEvent);
-
-        model.addAttribute("attendance", "休憩開始");
-        return "attendance-done";
-    }
-
-    // 休憩終了リクエスト
-    @PostMapping("/break/end")
-    public String endBreak(Model model) {
-        // 休憩終了時間を登録
-        TimeEvent timeEvent = new TimeEvent();
-        timeEvent.setEmployeeId("00000");
-        timeEvent.setEventType("BREAK_END");
-        registService.registAttendance(timeEvent);
-
-        model.addAttribute("attendance", "休憩終了");
-        return "attendance-done";
-    }
-
+    /**
+     * 勤怠登録
+     *
+     * @param eventTypeFormDto
+     * @param model
+     * @return 勤怠登録完了画面
+     */
     @PostMapping("/attendances")
     public String registAttendance(@ModelAttribute EventTypeFormDto eventTypeFormDto, Model model) {
         try {
