2669aafee72bc5a42cf6961568e450e17028ab84
Ryo Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2025-11-17 23:03:30 +0900

    enumを使ったvalidationを実装(#18)

--- src/main/java/com/kintai_manager/app/controller/AttendanceController.java
+++ src/main/java/com/kintai_manager/app/controller/AttendanceController.java
@@ -2,17 +2,20 @@ package com.kintai_manager.app.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.kintai_manager.app.entity.TimeEvent;
+import com.kintai_manager.app.form.EventTypeFormDto;
 import com.kintai_manager.app.service.RegistService;
+import com.kintai_manager.app.service.validation.EventTypeCheck;
 
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/kintai_manager/attendance")
+@RequestMapping("/kintai_manager/")
 public class AttendanceController {
 
     private final RegistService registService;
@@ -66,6 +69,23 @@ public class AttendanceController {
         registService.registAttendance(timeEvent);
 
         model.addAttribute("attendance", "休憩終了");
+        return "attendance-done";
+    }
+
+    @PostMapping("/attendances")
+    public String registAttendance(@ModelAttribute EventTypeFormDto eventTypeFormDto, Model model) {
+        try {
+            EventTypeCheck eventTypeCheck = new EventTypeCheck();
+            eventTypeCheck.checkEventType(eventTypeFormDto);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("不正な勤怠種類です。入力値： " + eventTypeFormDto.getEventType());
+        }
+        TimeEvent timeEvent = new TimeEvent();
+        timeEvent.setEmployeeId("00000");
+        timeEvent.setEventType(eventTypeFormDto.getEventType());
+        registService.registAttendance(timeEvent);
+
+        model.addAttribute("attendance", "勤務開始");
         return "attendance-done";
     }
 }
