a21f6e7cfda9cccced001a03b3d60521d93351d8
Ryo Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2025-11-20 07:01:32 +0900

    指定月の情報取得インタフェース作成(#17)

---
+++ src/main/java/com/kintai_manager/app/service/CreateMonthlyCalendarServiceImpl.java
@@ -0,0 +1,19 @@
+package com.kintai_manager.app.service;
+
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+
+import com.kintai_manager.app.dto.MonthlyCalendarResult;
+
+public class CreateMonthlyCalendarServiceImpl implements CreateMonthlyCalendarService {
+    @Override
+    public MonthlyCalendarResult createMonthlyCalendar(String targetMonth) {
+
+        DateTimeFormatter inputFormat = DateTimeFormatter.ofPattern("uuuuMM");
+        YearMonth yearMonth = YearMonth.parse(targetMonth, inputFormat);
+
+        MonthlyCalendarResult monthlyCalendarResult = new MonthlyCalendarResult();
+        monthlyCalendarResult.setEndDate(yearMonth.atEndOfMonth().format(outputFormat));
+        return monthlyCalendarResult;
+    }
+}
