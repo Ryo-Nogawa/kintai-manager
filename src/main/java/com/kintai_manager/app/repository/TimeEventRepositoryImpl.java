aa703ba697259ba36d50490c59d06dc234f31c17
Ryo Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2025-11-19 20:25:30 +0900

    勤怠データ取得用SQL作成(#17)

--- src/main/java/com/kintai_manager/app/repository/TimeEventRepositoryImpl.java
+++ src/main/java/com/kintai_manager/app/repository/TimeEventRepositoryImpl.java
@@ -1,5 +1,9 @@
 package com.kintai_manager.app.repository;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -15,8 +19,6 @@ public class TimeEventRepositoryImpl implements TimeEventRepository {
 
     @Override
     public void addTimeEvent(TimeEvent timeEvent) {
-        System.out.println("Databaseへ登録します。");
-
         String request_sql = "INSERT INTO time_event "
                 + " (employee_id, event_at, event_type, updated_at, updated_employee_id, created_at, created_employee_id) "
                 + " VALUES (?, DATE_FORMAT(NOW(), '%Y%m%d%H%i'), ?, CURRENT_TIMESTAMP, ?, CURRENT_TIMESTAMP, ?)";
@@ -25,5 +27,33 @@ public class TimeEventRepositoryImpl implements TimeEventRepository {
         // time_eventテーブルにデータを登録
         jdbcTemplate.update(request_sql,
                 employee_id, timeEvent.getEventType(), employee_id, employee_id);
+    }
+
+    @Override
+    public List<TimeEvent> getTimeEvents(String employeeId, String targetMonth) {
+        // SQL文を作成
+        String request_sql = ""
+                + "SELECT "
+                + "event_at, "
+                + "event_type "
+                + "FROM time_event "
+                + "WHERE employee_id = ? "
+                + "AND event_at LIKE ?||'%' "
+                + "ORDER BY event_at;";
+
+        // queryForListメソッドでSQL文を実行し、結果MapのListで受け取る
+        List<Map<String, Object>> timeEvents = jdbcTemplate.queryForList(request_sql, employeeId, targetMonth);
+
+        // TimeEvent格納用のList作成
+        List<TimeEvent> timeEventList = new ArrayList<>();
+
+        // 受け取ったMapのListをfor文で回し、各勤怠データをTimeEventオブジェクトに格納
+        for (Map<String, Object> timeEvent : timeEvents) {
+            TimeEvent timeEventEntity = new TimeEvent();
+            timeEventEntity.setEventAt((String) timeEvent.get("event_at"));
+            timeEventEntity.setEventType((String) timeEvent.get("event_type"));
+            timeEventList.add(timeEventEntity);
+        }
+        return timeEventList;
     }
 }
