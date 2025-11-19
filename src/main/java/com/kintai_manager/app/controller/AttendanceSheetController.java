b57a9b187b9e1332b59e01c68b4fb07d719bbe9b
Ryo Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2025-11-19 19:55:17 +0900

    クエリパラメータのバリデーションチェック(#17)

--- src/main/java/com/kintai_manager/app/controller/AttendanceSheetController.java
+++ src/main/java/com/kintai_manager/app/controller/AttendanceSheetController.java
@@ -1,20 +1,27 @@
 package com.kintai_manager.app.controller;
 
+import java.time.format.DateTimeParseException;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import com.kintai_manager.app.entity.TimeEvent;
+import com.kintai_manager.app.service.validation.TargetMonthCheck;
 
 @Controller
 @RequestMapping("/kintai_manager/attendance/sheet/")
 public class AttendanceSheetController {
 
-    @GetMapping("index")
-    public String index() {
-        // 勤務シートのトップページを表示
-        TimeEvent timeEvent = new TimeEvent();
-        timeEvent.setEmployeeId("00000");
-        return "attendance-sheet/index";
+    @GetMapping("show")
+    public String attendanceSheetShow(@RequestParam String targetMonth, Model model) {
+        try {
+            TargetMonthCheck targetMonthCheck = new TargetMonthCheck();
+            targetMonthCheck.checkTargetMonth(targetMonth);
+        } catch (DateTimeParseException e) {
+            throw new IllegalArgumentException("不正な対象月です。入力値： " + targetMonth);
+        }
+        return "attendance-sheet/show";
     }
 }
