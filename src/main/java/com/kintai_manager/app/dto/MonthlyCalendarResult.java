c13c5b1fa95f9ffe5bb4823d51a07aeb0345483c
Ryo Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2025-11-22 12:03:23 +0900

    勤怠情報取得結果を格納するフィールドを追加(#17)

--- src/main/java/com/kintai_manager/app/dto/MonthlyCalendarResult.java
+++ src/main/java/com/kintai_manager/app/dto/MonthlyCalendarResult.java
@@ -2,12 +2,17 @@ package com.kintai_manager.app.dto;
 
 import java.util.List;
 
+import com.kintai_manager.app.entity.TimeEvent;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 public class MonthlyCalendarResult {
+    // 勤怠情報リスト
+    private List<TimeEvent> timeEvents;
+
     // 月末日
     private int endDate;
 
